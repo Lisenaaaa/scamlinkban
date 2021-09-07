472020c4d6dc1872b230da5e55fe39907a9ae5d8
Lisena <lisenawastaken@gmail.com>
2021-09-06 19:31:49 -0700

    update to lis and bushbot

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# zordlans scam link ban plugin
+# lis's scam link ban plugin
 
-needs https://inv.wtf/bot to work, and it needs the default prefix
+why are you looking at this lol
--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const { Plugin } = require('powercord/entities');
 module.exports = class scamLinkBan extends Plugin {
   async startPlugin () {
     powercord.api.commands.registerCommand({
-      command: 'steamscamban',
+      command: 'scamlinkban',
       description: 'bans a user for scam link',
       usage: '{c} [user id or ping]',
       executor: (args) => ({
         send: true,
-        result: `fire ban ${args} sending a scam link --days 1`
+        result: `-dban ${args} sending a scam link --days 1`
       })
     });
   }
 
   pluginWillUnload () {
-    powercord.api.commands.unregisterCommand('steamscamban');
+    powercord.api.commands.unregisterCommand('scamlinkban');
   }
 };
